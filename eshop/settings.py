c8f03f4a2ac904e0f0e9fc0133895117376782e3
Oleksandr Podynihlazov <x00215403@mytudublin.ie>
2024-11-28 22:17:25 +0000

    create sign up, sign out, login logic

--- eshop/settings.py
+++ eshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'accounts',
     'crispy_forms',
     'crispy_bootstrap5',
 ]
@@ -132,6 +133,9 @@ MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'shop:cat_list'
+LOGOUT_REDIRECT_URL = 'shop:cat_list'
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
